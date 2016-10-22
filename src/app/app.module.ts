48c4a24dcf0ab8b7922f6ae75680bdd22b0208dc
Radu Vultur <raduvultur@Radus-MacBook-Pro.local>
2016-10-22 17:19:36 +0200

    2nd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+import { ConnectivityService } from '../providers/connectivity-service';
+import { InstagramService } from '../providers/instagram-service';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,6 @@ import { Page2 } from '../pages/page2/page2';
     Page1,
     Page2
   ],
-  providers: []
+  providers: [ConnectivityService, InstagramService]
 })
 export class AppModule {}
--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,7 +1,8 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Geolocation } from 'ionic-native';
 import { InstagramService } from '../../providers/instagram-service';
+import { ConnectivityService } from '../../providers/connectivity-service';
+import { Geolocation } from 'ionic-native';
 
 declare var google;
 
@@ -16,58 +17,141 @@ export class Page1 {
 	@ViewChild('map') mapElement: ElementRef;
 	map: any;
 	public instaPics: any;
+	mapInitialised: boolean = false;
+  	apiKey: any = "AIzaSyBPZOuQNePXO9izuGtazWZuRHwkeRMi4bE";
 
-  constructor(public navCtrl: NavController,public instagramService: InstagramService) {
-    
+  constructor(public navCtrl: NavController,public instagramService: InstagramService, public connectivityService: ConnectivityService) {
+  	this.loadGoogleMaps();
   }
 
-	ionViewDidLoad(){
-    	this.loadMap();
-  	}
+  loadGoogleMaps(){
  
-  	loadMap(){
+    this.addConnectivityListeners();
  
- 		//http://stackoverflow.com/questions/4130237/displaying-crosshairs-in-the-center-of-a-javascript-google-map
-
-	    Geolocation.getCurrentPosition().then((position) => {
+  	if(typeof google == "undefined" || typeof google.maps == "undefined"){
+ 
+	    console.log("Google maps JavaScript needs to be loaded.");
+	    this.disableMap();
 	 
-	      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+	    if(this.connectivityService.isOnline()){
+	      console.log("online, loading map");
 	 
-	      let mapOptions = {
-	        center: latLng,
-	        zoom: 15,
-	        mapTypeId: google.maps.MapTypeId.ROADMAP
+	      //Load the SDK
+	      window['mapInit'] = () => {
+	        this.initMap();
+	        this.enableMap();
 	      }
 	 
-	      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-	    }, (err) => {
-
-	    	let latLng = new google.maps.LatLng(48.874669, 2.229712);
+	      let script = document.createElement("script");
+	      script.id = "googleMaps";
 	 
-	      	let mapOptions = {
-	        	center: latLng,
-	        	zoom: 15,
-	        	mapTypeId: google.maps.MapTypeId.ROADMAP
-	      	}
+	      if(this.apiKey){
+	        script.src = 'http://maps.google.com/maps/api/js?key=' + this.apiKey + '&callback=mapInit';
+	      } else {
+	        script.src = 'http://maps.google.com/maps/api/js?callback=mapInit';       
+	      }
 	 
-	      	this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+	 	  if (!document.getElementById('googleMaps')) {			    
+		  	console.log("Adding script: "+script.src);
+	      	document.body.appendChild(script); 
+		  } else {
+		  	console.log("Map script exists, skip add.");
+		  }
 
-			var crosshairShape = {coords:[0,0,0,0],type:'rect'};
-			var marker = new google.maps.Marker({
-			map: this.map,
-			icon: '/assets/img/cross-hairs.gif',
-			shape: crosshairShape
-			});
-  			marker.bindTo('position', this.map, 'center');
+	 	   
+	 
+	    } 
+	}
+	else {
+	 
+	    if(this.connectivityService.isOnline()){
+	      console.log("showing map");
+	      this.initMap();
+	      this.enableMap();
+	    }
+	    else {
+	      console.log("disabling map");
+	      this.disableMap();
+	    }
+	 
+	  }
+ 
+  }
+ 
 
-	     	console.log(err);
-	    });
+  initMap(){
+ 
+    this.mapInitialised = true;
+ 	let latLng = new google.maps.LatLng(48.874669, 2.229712);
 
-  	}
+    Geolocation.getCurrentPosition().then((position) => {
+      latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    }, (err) => {
+	  console.log("Error getting position: " + err);
+    });
+
+    let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    var crosshairShape = {coords:[0,0,0,0],type:'rect'};
+    var marker = new google.maps.Marker({
+	  map: this.map,
+	  icon: '/assets/img/cross-hairs.gif',
+	  shape: crosshairShape
+	});
+    marker.bindTo('position', this.map, 'center');
+ 
+  }
+
+
+  disableMap(){
+    console.log("disable map");
+  }
+ 
+  enableMap(){
+    console.log("enable map");
+  }
+
+
+  addConnectivityListeners(){
+ 
+    let onOnline = () => {
+ 
+      setTimeout(() => {
+        if(typeof google == "undefined" || typeof google.maps == "undefined"){
+ 
+          this.loadGoogleMaps();
+ 
+        } else {
+ 
+          if(!this.mapInitialised){
+            this.initMap();
+          }
+ 
+          this.enableMap();
+        }
+      }, 2000);
+ 
+    };
+ 
+    let onOffline = () => {
+      this.disableMap();
+    };
+ 
+    document.addEventListener('online', onOnline, false);
+    document.addEventListener('offline', onOffline, false);
+ 
+  }
 
   	showPics(){
   		// http://blog.ionic.io/handling-cors-issues-in-ionic/
   		let apiRequest = "http://localhost:8100/instagram/v1/media/search?lat="+this.map.getCenter().lat()+"&lng="+this.map.getCenter().lng()+"&distance=5000&access_token=31204544.010c112.4f7224ba859c481d960faf4c8a2303d5";
+  		//let apiRequest = "https://api.instagram.com/v1/media/search?lat="+this.map.getCenter().lat()+"&lng="+this.map.getCenter().lng()+"&distance=5000&access_token=31204544.010c112.4f7224ba859c481d960faf4c8a2303d5";
   		console.log(apiRequest); 
   		this.instagramService.load(apiRequest)
 		  .then(data => {
